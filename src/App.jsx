912a3d3055b23e17c810ad07dd4c2b2e421c4931
Patrick Lam <patricklam12345@gmail.com>
2023-09-07 20:59:45 -0400

    added components and fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Authenticate from "./components/Authenticate";
+import SignUpForm from "./components/SignUpForm";
 
 function App() {
   return (
     <>
+      <Authenticate />
+      <SignUpForm />
     </>
   )
 }
